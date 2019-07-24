1123aa7027f199406ffc6991627e692f40594675
Salomon BRYS <salomon@kodein.net>
2019-07-24 12:10:57 +0200

    isTrue & isExcluded become public API

--- src/main/kotlin/org/kodein/internal/gradle/utils.kt
+++ src/main/kotlin/org/kodein/internal/gradle/utils.kt
@@ -1,6 +1,5 @@
 package org.kodein.internal.gradle
 
-import android.databinding.tool.ext.toCamelCase
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.tasks.testing.AbstractTestTask
@@ -21,7 +20,7 @@ internal fun Project.configureTestLogsPrint() {
     }
 }
 
-internal fun Project.isTrue(property: String) = findProperty(property) == "true"
+fun Project.isTrue(property: String) = findProperty(property) == "true"
 
-internal fun Project.isExcluded(framework: String) =
+fun Project.isExcluded(framework: String) =
         (findProperty("exclude${framework.capitalize()}") ?: System.getenv("EXCLUDE_${framework.toUpperCase()}")) == "true"
