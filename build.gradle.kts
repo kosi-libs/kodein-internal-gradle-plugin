fe20f1c228ecdc78286e615f099b05431e3d3982
Romain Boisselle <romain@boisselle.dev>
2019-11-25 12:56:33 +0100

    Kotlin 1.3.60

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,7 +40,7 @@ dependencies {
 
 allprojects {
     group = "org.kodein.internal.gradle"
-    version = "2.9.6"
+    version = "2.9.7"
 
     afterEvaluate {
         val sourcesJar = task<Jar>("sourcesJar") {
--- kodein-internal-gradle-versions/src/main/kotlin/org/kodein/internal/gradle/KodeinVersions.kt
+++ kodein-internal-gradle-versions/src/main/kotlin/org/kodein/internal/gradle/KodeinVersions.kt
@@ -4,7 +4,7 @@ import org.gradle.api.*
 
 object KodeinVersions {
 
-    const val kotlin = "1.3.50"
+    const val kotlin = "1.3.60"
     const val androidBuildTools = "3.5.0-rc01"
 
 }
