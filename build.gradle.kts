4a976f77f32ecf842b8fa3c25d208379a46a49c5
Salomon BRYS <salomon@kodein.net>
2020-01-28 18:47:40 +0100

    Kotlin 1.3.70 EAP to have native working on IDEA.

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,7 +40,7 @@ dependencies {
 
 allprojects {
     group = "org.kodein.internal.gradle"
-    version = "2.10.4"
+    version = "2.10.4-eap"
 
     afterEvaluate {
         val sourcesJar = task<Jar>("sourcesJar") {
