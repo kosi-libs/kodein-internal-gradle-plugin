5c831624300fa1ef4a3dfca92795699794d6effc
Romain Boisselle <romain@boisselle.dev>
2021-02-23 14:18:45 +0100

    update release script

--- sonatype-rest-api/release-repository.sh
+++ sonatype-rest-api/release-repository.sh
@@ -32,7 +32,7 @@ then
       exit 1
 fi
 
-closing=$(
+closingRepository=$(
   curl -s --request POST -u "$username:$password" \
     --url https://oss.sonatype.org/service/local/staging/bulk/close \
     --header 'Accept: application/json' \
@@ -41,26 +41,49 @@ closing=$(
 )
   # todo discuss autoReleaseAfterClose
 
-if [ ! -z "$closing" ]; then
-    echo "error while closing $stagedRepositoryId : $closing"
+if [ ! -z "$closingRepository" ]; then
+    echo "error while closing repository $stagedRepositoryId : $closingRepository"
     exit 1
 fi
 
-type=
-transitioning=
-for i in {1..60} ; do
-  response=$(curl -s --request GET -u "$username:$password" \
-        --url https://oss.sonatype.org/service/local/staging/repository/"$stagedRepositoryId" \
+while true ; do
+  rules=$(curl -s --request GET -u "$username:$password" \
+        --url https://oss.sonatype.org/service/local/staging/repository/"$stagedRepositoryId"/activity \
         --header 'Accept: application/json' \
         --header 'Content-Type: application/json')
 
-  type=$(echo "$response" | jq -r '.type' )
-  transitioning=$(echo "$response" | jq -r '.transitioning' )
-  echo "$i type=$type / transitioning=$transitioning"
-  if [ "$type" = "closed" ] && [ "$transitioning" = "false" ]; then
+  closingRules=$(echo "$rules" | jq '.[] | select(.name=="close")')
+  if [ -z "$closingRules" ] ; then
+    continue
+  fi
+
+  rulesPassed=$(echo "$closingRules" | jq '.events | any(.name=="rulesPassed")')
+  rulesFailed=$(echo "$closingRules" | jq '.events | any(.name=="rulesFailed")')
+
+  if [ "$rulesFailed" = true ]; then
+    echo "Staged repository [$stagedRepositoryId] could not be closed."
+    exit 1
+  fi
+
+  if [ "$rulesPassed" = true ]; then
+        while true ; do
+            repository=$(curl -s --request GET -u "$username:$password" \
+              --url https://oss.sonatype.org/service/local/staging/repository/"$stagedRepositoryId" \
+              --header 'Accept: application/json' \
+              --header 'Content-Type: application/json')
+
+          type=$(echo "$repository" | jq -r '.type' )
+          transitioning=$(echo "$repository" | jq -r '.transitioning' )
+          echo "type=$type / transitioning=$transitioning"
+          if [ "$type" = "closed" ] && [ "$transitioning" = "false" ]; then
+              break
+          else
+              sleep 1
+          fi
+        done
       break
   else
-      sleep 10
+      sleep 5
   fi
 done
 
