d0709ce90f3f58353cc2576b87d6f63348b6c0ad
Salomon BRYS <salomon.brys@gmail.com>
2021-04-03 13:55:30 +0200

    Kotlin 1.4.32

--- kodein-internal-gradle-versions/src/main/kotlin/org/kodein/internal/gradle/KodeinVersions.kt
+++ kodein-internal-gradle-versions/src/main/kotlin/org/kodein/internal/gradle/KodeinVersions.kt
@@ -4,8 +4,8 @@ import org.gradle.api.*
 
 object KodeinVersions {
 
-    const val kotlin = "1.4.31"
-    const val dokka = "1.4.20.2-dev-74"
+    const val kotlin = "1.4.32"
+    const val dokka = "1.4.30"
     const val androidBuildTools = "4.0.2"
     const val androidNdk = "21.3.6528147"
 
