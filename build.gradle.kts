630581c5541c8c90badfda084bf4f26da827ace5
Romain BOISSELLE <romain.boisselle@proton.me>
2022-07-17 14:25:59 +0200

    Kotlin 1.7.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ buildscript {
 
 allprojects {
     group = "org.kodein.internal.gradle"
-    version = "6.19.0"
+    version = "6.20.0"
 }
 
 repositories {
--- kodein-internal-gradle-versions/src/main/kotlin/org/kodein/internal/gradle/KodeinVersions.kt
+++ kodein-internal-gradle-versions/src/main/kotlin/org/kodein/internal/gradle/KodeinVersions.kt
@@ -5,8 +5,8 @@ import org.gradle.api.*
 @Suppress("unused")
 object KodeinVersions {
 
-    const val kotlin = "1.7.0"
-    const val dokka = "1.7.0"
+    const val kotlin = "1.7.10"
+    const val dokka = "1.7.10"
     const val androidBuildTools = "7.0.0"
     const val androidNdk = "21.3.6528147"
 
